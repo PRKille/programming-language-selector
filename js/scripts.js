7e60d0e24990c5eca4bda0b3206084b847746ab2
Patrick Kille <pr.kille@gmail.com>
2020-01-17 10:58:02 -0800

    change javascript function in anticipation of html rewrite.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,16 @@
 $(document).ready(function(){
-  var answerOne;
-  var answerTwo = $("input:radio[name=question2]:checked").val();
-  var answerThree = $("input:radio[name=question3]:checked").val();
-  var answerFour = $("input:radio[name=question4]:checked").val();
-  var answerFive = parseInt($("input:radio[name=question5]:checked").val());
   $("form#form-one").submit(function(){
-    $("#form-two").show();
     $("#form-one").hide();
     event.preventDefault();
     answerOne = $("input:radio[name=question1]:checked").val();
     console.log(answerOne);
-  });
-  $("form#form-two").submit(function(){
-    $("#form-three").show();
-    $("#form-two").hide();
-    event.preventDefault();
+    answerTwo = $("input:radio[name=question2]:checked").val();
     console.log(answerTwo);
+    answerThree = $("input:radio[name=question3]:checked").val();
+    console.log(answerThree);
+    answerFour = $("input:radio[name=question4]:checked").val();
+    console.log(answerFour);
+    answerFive = $("input:radio[name=question5]:checked").val();
+    console.log(answerFive);
   });
-  console.log(answerTwo);
-
-  var globalString = "This is a global variable";
-
-function sampleFunction() {
-  alert(globalString);
-  globalString = "This is a global variable update!!";
-  console.log(globalString);
-  alert(globalString);
-}
-
-alert(globalString);
-sampleFunction();
-alert(globalString);
 });
